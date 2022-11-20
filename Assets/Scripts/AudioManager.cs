ceb0a6dfdfe8ccb1784d89a3812041ca34777dcd
Zaatar <hani.zaatar.92@gmail.com>
2022-11-20 21:19:14 +0100

    Adding Audio files to project and creating AudioManager prefab and Sound class

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -14,13 +14,34 @@ public class AudioManager : MonoBehaviour
             s.source.clip = s.clip;
             s.source.volume = s.volume;
             s.source.pitch = s.pitch;
-
+            s.source.loop = s.loop;
         }
+    }
+
+    void Start()
+    {
+        Play("Theme");
     }
 
     public void Play (string name)
     {
         Sound s = Array.Find(sounds, sound => sound.name == name);
+        if (s == null)
+        {
+            Debug.LogWarning("Sound: " + name + " not found");
+            return;
+        }
         s.source.Play();
+    }
+
+    public void Stop(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        if (s == null)
+        {
+            Debug.LogWarning("Sound: " + name + " not found");
+            return;
+        }
+        s.source.Stop();
     }
 }
